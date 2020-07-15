764119ebaaf76d6edd6a270b38ede64874771569
Shubham Semwal <shubham.semwal11@gmail.com>
2020-07-15 17:19:18 +0530

    Initial Commit

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = 'prHTaaTrd4BeeSjQgV5eMnbOvSKZhZNm';
+
+//get weather info
+const getWeather = async(id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+//get city info
+const getCity = async(city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query =`?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
